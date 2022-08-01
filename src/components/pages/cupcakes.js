f0825973d76c9787fa38bbf538a9897af824c491
Felipe Romero <tmfromero@gmail.com>
2022-08-01 19:00:23 -0400

    Se crea el hook personalizado useFetch y se agrega el atributo error para detectar errores.

--- src/components/pages/cupcakes.js
+++ src/components/pages/cupcakes.js
@@ -1,18 +1,21 @@
-import { useEffect, useState } from "react"
-import {get} from "axios"
 import Cupcake from "../../cards/Cupcake"
+import useFetch from "../../hooks/useFetch"
 
 const Cupcakes = ({peticion, title}) => {
+    // Esto se utuliza antes del hook creado useFetch
+    // const [cupcakes, setCupcakes] = useState([])
 
-    const [cupcakes, setCupcakes] = useState([])
+    // useEffect(() => {
+    //     get(`${process.env.REACT_APP_URL_API}${peticion}`)
+    //         // .then(response => response.json())    //Esta línea la utiliza fetch
+    //         .then(({data}) => setCupcakes(data))
+    //         .catch(e => console.log(e))
+    // }, [peticion])
 
-    useEffect(() => {
-        get(`${process.env.REACT_APP_URL_API}${peticion}`)
-            // .then(response => response.json())    //Esta línea la utiliza fetch
-            .then(({data}) => setCupcakes(data))
-            .catch(e => console.log(e))
-    }, [peticion])
-
+    const [cupcakes, error] = useFetch (peticion)
+    if (error) {
+        return <span>Hubo un error</span>
+    }
     return (
         <div className="ed-grid">
             { title && <h1>Página de Cupcakes</h1>}
--- src/components/sections/Servicios.js
+++ src/components/sections/Servicios.js
@@ -1,17 +1,22 @@
-import { useEffect, useState } from "react"
-import { get } from "axios"
+import useFetch from "../../hooks/useFetch"
 
 const Servicios = ({peticion}) => {
-    const [servicios, setServicios] = useState()
+    // Esto se utilizaba antes de crear el hook useFetch
+    // const [servicios, setServicios] = useState()
 
-    useEffect (() => {
-        get (`${process.env.REACT_APP_URL_API}${peticion}`)
-            .then(({data}) => setServicios(data))
-            .catch(e => console.log(e))
-    }, [peticion])
+    // useEffect (() => {
+    //     get (`${process.env.REACT_APP_URL_API}${peticion}`)
+    //         .then(({data}) => setServicios(data))
+    //         .catch(e => console.log(e))
+    // }, [peticion])
+
+    const [servicios, error] = useFetch (peticion)
 
     if (!servicios){
         return <span>No hay servicios</span>
+    }
+    if (error) {
+        return <span>Hubo un error</span>
     }
     return (
         <div className="ed-grid">{
