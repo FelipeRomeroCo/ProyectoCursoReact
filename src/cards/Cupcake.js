cd52ebc105902e873de96f8aa7d0f04bc0444976
Felipe Romero <tmfromero@gmail.com>
2022-08-01 23:11:05 -0400

    Se incluyen los PropTypes

--- src/cards/Cupcake.js
+++ src/cards/Cupcake.js
@@ -1,12 +1,28 @@
+import {number, string} from "prop-types"
+
 const Cupcake = ({imagen, sabor, descripcion, color, precio}) => {
     return (
         <div className="s-radius-1 s-shadow-bottom background s-shadow-card-micro white-color s-pxy-2">
             <img src={imagen} alt={sabor}/>
             <p>{descripcion}</p>
             <span>Color: {color}</span>
-            <span>Precio:{ precio}</span> 
+            <br/>
+            <span>Precio: {precio}</span> 
         </div>
     )
+}
+
+Cupcake.propTypes = {
+    precio: number,
+    imagen: string.isRequired,
+    sabor: string.isRequired,
+    descripcion: string.isRequired,
+    color: string
+}
+
+Cupcake.defaultProps = {
+    imagen: "https://picsum.photos/200/200",
+    precio: 0
 }
 
 export default Cupcake
